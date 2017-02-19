858de669263714fd8db96cf29a8e0afb6db00c5b
Isaac Rhett <isaacjrhett@gmail.com>
2017-02-18 16:55:15 -0800

    improving client functionality

--- source/connect_to_server.c
+++ source/connect_to_server.c
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   close_clients.c                                    :+:      :+:    :+:   */
+/*   connect_to_server.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/18 15:00:35 by irhett            #+#    #+#             */
-/*   Updated: 2017/02/18 16:54:01 by irhett           ###   ########.fr       */
+/*   Created: 2017/02/18 16:49:13 by irhett            #+#    #+#             */
+/*   Updated: 2017/02/18 16:53:33 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pingpong.h"
 
-void	close_clients(s_srv *srv)
+int		connect_to_server(char *ip, char *port)
 {
-	s_cli	*tmp;
+	int		sockfd;
 
-	ft_putendl("Starting to close clients.");
-	if (srv)
-	{
-		while ((*srv).first)
-		{
-			ft_putendl("Closing client connection.");
-			if ((*(*srv).first).sockfd >= 0)
-				close((*(*srv).first).sockfd);
-			tmp = (*srv).first;
-			(*srv).first = (*(*srv).first).next;
-			free(tmp);
-		}
-	}
-	ft_putendl("All clients closed.");
+	sockfd = open_inet_socket();
+	if (sockfd < 0)
+		error("opening socket.");
+	if (connect_to_ip(ip, port, sockfd) < 0)
+		error("connecting to server.");
+	ft_putendl("Connected to server.");
+	return (sockfd);
 }
