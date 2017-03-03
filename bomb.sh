bf62308f6ef29c67f9f41307691452b9581f4f38
root <root@dubious>
2017-03-03 14:27:54 -0800

    fixed the bomb to be more... bomb-y

--- bomb.sh
+++ bomb.sh
@@ -1,9 +1,13 @@
-#[0;30m
 A="[1;"
+#[2A
 B="31m*"
+#[2A
 C="explod"
+#[2A
 D="es*[0m"
+#[2A
 echo $A$B$C$D
+#[2A
 exit
-[8A
-[0;31m*explodes*[0m
+[2A
+[0;31m*explodes anyway*[0m
--- schedule.sh
+++ schedule.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SCRIPT="bomb.sh"
 TIME=""
-TEST="2:13 PM 3/3/2017"
+TEST="2:17 PM 3/3/2017"
 
 apt-get install at -y
 
-cat "$SCRIPT" | at $TIME
+cat -v "$SCRIPT" | at $TEST
