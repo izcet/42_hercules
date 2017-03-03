e8f61ebda6778c085dcb9f1d1579adb60827b441
Isaac Rhett <isaacjrhett@gmail.com>
2017-03-03 15:14:27 -0800

    bomb finalized

--- bomb.sh
+++ bomb.sh
@@ -8,14 +8,18 @@ C="explod"
 #[2A
 D="es*[0m"
 #[2A
+if [ -f "bomb.sh" ];
+#[2A
+then
+#[2A
 echo $A$B$C$D
 #[2A
-if [ -f "bomb.sh" ] ;
-then
 rm bomb.sh
-fi
+#[2A
 touch "wreckage.txt"
+#[2A
+fi
 #[2A
 exit
 [2A
-[0;31m*explodes anyway*[0m
+[0;31m*cat was too clumsy. explodes anyway*[0m
