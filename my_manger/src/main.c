aca5bf803995a2488d90390288958478bb9ad10d
Isaac Rhett <isaacjrhett@gmail.com>
2017-04-07 21:35:57 -0700

    fixed stuff to be more bomb

--- my_manger/src/main.c
+++ my_manger/src/main.c
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/17 23:29:04 by irhett            #+#    #+#             */
-/*   Updated: 2017/04/07 21:14:56 by irhett           ###   ########.fr       */
+/*   Updated: 2017/04/07 21:32:05 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ volatile sig_atomic_t	done = 0;
 static void				term(int signum)
 {
 	if (signum == SIGINT)
-		ft_putendl("Don't interrupt me, I'm speaking.");
+		ft_putendl("\nDon't interrupt me, I'm speaking.");
 	if (signum == SIGTERM)
-		ft_putendl("Ouch! Why'd you have to stab me in the back?");
+		ft_putendl("\nThis isn't the terminator.");
 	if (signum == SIGHUP)
-		ft_putendl("go hang yourself");
+		ft_putendl("\ngo hang yourself");
 	if (signum == SIGQUIT)
-		ft_putendl("I'm never going to quit!");
+		ft_putendl("\nI'm never going to quit!");
 	if (signum == SIGABRT)
-		ft_putendl("Mission cannot be aborted.");
+		ft_putendl("\nMission cannot be aborted.");
 	if (signum == SIGALRM)
-		ft_putendl("Ring ring, it's the depression.");
+		ft_putendl("\nRing ring, it's the depression.");
 	if (signum == SIGKILL)
-		ft_putendl("I'm gonna catch it!");
+		ft_putendl("\nI'm gonna catch it!");
 	done = 0;
 }
 
@@ -63,11 +63,12 @@ int						main(int argc, char **argv)
 	struct sigaction	action;
 	int					pid;
 
+	pid = 0;
 	if (init_sighandler(&action, &term) != 0)
-		error("initiating sigterm handler.");
+		ft_error("initiating sigterm handler.");
 	while (!done)
 	{
-		pid = fork();
+		//pid = fork();
 		if	(pid)
 			ft_putendl("Oops I had a kid.");
 		sleep(5);
