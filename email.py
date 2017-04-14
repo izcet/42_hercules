21e9cd04f5e0201994af0620fa02e57f2bb2546e
Isaac Rhett <isaacjrhett@gmail.com>
2017-04-14 15:13:28 -0700

    using sendgrid, just implemented api. about to run test

--- email.py
+++ email.py
@@ -6,7 +6,7 @@
 #    By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/04/14 14:25:37 by irhett            #+#    #+#              #
-#    Updated: 2017/04/14 14:52:54 by irhett           ###   ########.fr        #
+#    Updated: 2017/04/14 15:12:57 by irhett           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,26 +14,26 @@ import smtplib											# AB
 from email.MIMEMUltipart import MIMEMultipart			# B
 from email.MIMEText import MIMEText						# B
 
-fromaddr = "MY ADDR"									# AB
-toaddr = "TARGET ADDR"									# B
+fromaddr = "apikey"										# AB
+key = "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"
+toaddr = "nartnetworks@grr.la"							# B
 msg = MIMEMultipart()									# B
 msg['From'] = fromaddr									# B
 msg['To'] = toaddr										# B
-msg['Subject'] = "Subject"								# B
+msg['Subject'] = "Test Message"							# B
 
 body = "Message Body"									# B
 msg.attach(MIMEText(body, 'plain'))						# B
 
-
-server = smtplib.SMTP('???.gmail.co?', 587)				# AB
+server = smtplib.SMTP('smtp.sendgrid.net', 587)			# AB
 server.starttls()										# AB
-server.login(fromaddr, "YOUR PASSWORD")					# AB
+server.login(fromaddr, key)								# AB
 
 text = msg.as_string()									# B
 
-msg_a = "YOUR MESSAGE!"									# A
+#msg_a = "YOUR MESSAGE!"								# A
 
-server.sendmail("YOUR EMAIL", "EMAIL TARGET", msg_a)	# A
-server.sendmail("YOUR EMAIL", "EMAIL TARGET", text)		# B
+#server.sendmail("YOUR EMAIL", "EMAIL TARGET", msg_a)	# A
+server.sendmail("user@website.co", toaddr, text)		# B
 
 server.quit()											# AB
