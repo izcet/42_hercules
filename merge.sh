2622d2825cd328b0ea66a13ae6486a861695fe98
Isaac Rhett <isaacjrhett@gmail.com>
2017-04-14 19:25:32 -0700

    fixed merge script and used it to copy girdle_of_hippolyta

--- merge.sh
+++ merge.sh
@@ -3,6 +3,8 @@
 PROJ_DIR=projects/
 CURR=$1
 
+BACK=~/backups/projects/hercules
+
 FULL=~/$PROJ_DIR$CURR
 
 if [ ! -d $FULL ] ; then
@@ -10,18 +12,30 @@ if [ ! -d $FULL ] ; then
 	exit
 fi
 
-if [ -n $CURR ] ; then
+if [ -z $CURR ] ; then
 	echo "project path cannot be NULL"
 	exit
 fi
 
-# mkdir $FULL/$CURR
-# mv -rf $FULL/!($CURR) $FULL/$CURR/
-# git commit -a -m "Preparing project $CURR for move"
+mkdir $FULL/$CURR
+mv -f $FULL/* $FULL/$CURR/
 
-# git remote add temp $FULL
-# git fetch temp
-# git merge temp/master
-# git rm temp
+cd $FULL
+git add .
+git commit -a -m "Preparing project $CURR for move"
 
+cd $BACK
+git remote add temp $FULL
+git fetch temp
+git merge temp/master
+git rm temp
+gp
+
+cd $FULL
+mv -f $CURR/* .
+rm -r $CURR
+git add .
+git commit -a -m "Project $CURR moved and restored"
+
+cd $BACK
 exit
