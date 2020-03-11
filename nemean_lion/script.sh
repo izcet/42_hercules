1c0609f94a453e9e72f0a314497f3217a30a04e2
Ilya Klimov <53608243+Ilya2000Klimov@users.noreply.github.com>
2020-03-10 21:16:04 -0700

    script.sh
    You need to de-comet the line in order for the port to change, otherwhise the machine will not look at the comented out text, and run the default port.

--- nemean_lion/script.sh
+++ nemean_lion/script.sh
@@ -3,7 +3,7 @@
 # must be run as root
 apt-get install openssh-server
 
-cat /etc/ssh/sshd_config | sed 's/Port 22/Port 4242/' > .my_sshd_config
+cat /etc/ssh/sshd_config | sed 's/# Port 22/Port 4242/' > .my_sshd_config
 
 mv .my_sshd_config /etc/ssh/sshd_config
 
